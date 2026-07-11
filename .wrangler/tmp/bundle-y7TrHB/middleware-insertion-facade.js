41fdf786956e3b9b94a6e3d02688f3fa26776989
Mastermanikant <mastermanikant.in@gmail.com>
2026-07-11 07:02:50 +0530

    last update 11 7 26 7 2 am

---
+++ .wrangler/tmp/bundle-y7TrHB/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "D:\\Englishvidya\\Website_Source\\.wrangler\\tmp\\pages-EBqWQw\\functionsWorker-0.2598488561489629.mjs";
+				import * as __MIDDLEWARE_0__ from "D:\\Englishvidya\\Website_Source\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "D:\\Englishvidya\\Website_Source\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "D:\\Englishvidya\\Website_Source\\.wrangler\\tmp\\pages-EBqWQw\\functionsWorker-0.2598488561489629.mjs";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
